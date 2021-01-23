8a5c1685eb87631e1612a4a6d1b5365ed6c6ea53
Nils Wittler <nilswe@posteo.de>
2021-01-23 21:20:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Trollface.js
+
+Brings the :trollface: to your website!
+
+## Usage
+
+Either do it the oldschool way `<script src="trollfacejs.js"></script>` or import it in your app via `import 'trollfacejs'`.
+
+Call `[window.]trollfacejs.start()` to display that sexy tollface and `[window.]trollfacejs.stop()` to hide again.
+
+Lots of fun incoming!
---
+++ src/trollfacejs.ts
@@ -0,0 +1,132 @@
+import trollface from './assets/trollface';
+
+type TrollFace = {
+  start: () => void;
+  stop: () => void;
+};
+
+declare global {
+  interface Window {
+    trollfacejs: TrollFace;
+  }
+}
+
+if (typeof window === 'undefined') {
+  throw new Error('needs to run in the browser context');
+}
+
+type StyleProps = {
+  position?: string;
+  left?: number;
+  bottom?: number;
+  transform?: {
+    translate3d: {
+      x: number;
+      y: number;
+    };
+  };
+  opacity?: number;
+};
+
+type SlideDir = 'right' | 'left';
+
+const determainSlideDir = (currentDir: SlideDir, leftOffset: number): SlideDir => {
+  if (currentDir === 'right' && leftOffset >= window.innerWidth) {
+    return 'left';
+  } else if (currentDir === 'left' && leftOffset <= 200) {
+    return 'right';
+  }
+  return currentDir;
+};
+
+const TrollFaceJS = () => {
+  let animationFrameRef: number = null;
+  let trollfaceRef: HTMLOrSVGImageElement = null;
+
+  const animate = () => {
+    const stylesBaseValues: StyleProps = {
+      position: 'fixed',
+      left: 0,
+      bottom: 0,
+      transform: {
+        translate3d: {
+          x: 0,
+          y: 50,
+        },
+      },
+      opacity: 1,
+    };
+    let prevStyles: StyleProps = null;
+    let slideDir: SlideDir = 'right';
+
+    // for the first run
+    if (!prevStyles) {
+      prevStyles = stylesBaseValues;
+    }
+
+    const setPosition = () => {
+      const tfRect = trollfaceRef.getBoundingClientRect();
+      const leftOffset = prevStyles.transform.translate3d.x + tfRect.width;
+
+      const newSlideDir = determainSlideDir(slideDir, leftOffset);
+      slideDir = newSlideDir;
+
+      const translateXVal =
+        slideDir === 'left' ? prevStyles.transform.translate3d.x - 18 : prevStyles.transform.translate3d.x + 18;
+
+      stylesBaseValues.transform.translate3d.x = translateXVal;
+
+      const styles = {
+        position: `${stylesBaseValues.position}`,
+        left: `${stylesBaseValues.left}px`,
+        bottom: `${stylesBaseValues.bottom}`,
+        transform: `translate3d(${stylesBaseValues.transform.translate3d.x}px,${stylesBaseValues.transform.translate3d.y}%,0)`,
+        opacity: `${stylesBaseValues.opacity}`,
+      };
+
+      for (const styleProp in styles) {
+        trollfaceRef.style[styleProp] = styles[styleProp];
+      }
+
+      prevStyles = stylesBaseValues;
+      animationFrameRef = requestAnimationFrame(setPosition);
+    };
+
+    animationFrameRef = requestAnimationFrame(setPosition);
+  };
+
+  const attachFace = () => {
+    const body = document.querySelector('body');
+
+    if (!body) {
+      throw new Error('could not find that body - wtf?!');
+    }
+
+    if (document.querySelector('#thetrollface')) {
+      throw new Error('trollface is already attached!');
+    }
+
+    body.insertAdjacentHTML('beforeend', trollface);
+    trollfaceRef = document.querySelector('#thetrollface');
+
+    animate();
+  };
+
+  const killFace = () => {
+    cancelAnimationFrame(animationFrameRef);
+    trollfaceRef.remove();
+  };
+
+  return {
+    start: () => {
+      attachFace();
+    },
+    stop: () => {
+      killFace();
+    },
+  };
+};
+
+window.trollfacejs = TrollFaceJS();
+
+export {};
